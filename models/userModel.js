eb7db40be5b66eff3d2920dc6d5e7f25df10a4aa
Peter <hlopyannikov@gmail.com>
2017-10-10 21:02:02 +0300

    asd

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-//import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
@@ -21,36 +21,36 @@ const UserModel = new Schema({
   social: { type: String, required: false },
   avatar: { type: String, required: true, }
 });
-//
-// UserModel.pre("save", function (next) {
-//   const user = this;
-//   if (this.isModified("password") || this.isNew) {
-//     bcrypt.genSalt(10, (err, salt) => {
-//       if (err) {
-//         return next(err);
-//       }
-//       bcrypt.hash(user.password, salt, (error, hash) => {
-//         if (error) {
-//           return next(error);
-//         }
-//         user.password = hash;
-//         next();
-//       });
-//     });
-//   } else {
-//     return next();
-//   }
-// });
 
-// // Create method to compare password input to password saved in database
-// UserModel.methods.comparePassword = function (pw, cb) {
-//   bcrypt.compare(pw, this.password, (err, isMatch) => {
-//     if (err) {
-//       return cb(err);
-//     }
-//     cb(null, isMatch);
-//   });
-// };
+UserModel.pre("save", function (next) {
+  const user = this;
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, (error, hash) => {
+        if (error) {
+          return next(error);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+// Create method to compare password input to password saved in database
+UserModel.methods.comparePassword = function (pw, cb) {
+  bcrypt.compare(pw, this.password, (err, isMatch) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", UserModel, "users");
 export default User;
